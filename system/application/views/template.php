783f21d17b515fdb9f51670e214b00cdc8a5b4a0
Jim Kalafut <devnull@localhost>
2010-10-04 17:18:53 -0700

    Added a template view

---
+++ system/application/views/template.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+	"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<link rel="stylesheet" href="/css/blueprint/screen.css" type="text/css" media="screen, projection">
+<link rel="stylesheet" href="/css/blueprint/print.css" type="text/css" media="print">	
+<!--[if lt IE 8]><link rel="stylesheet" href="/css/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
+
+<link type="text/css" href="/css/sunny/jquery-ui-1.8.2.custom.css" rel="stylesheet">	
+<script type="text/javascript" src="/js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="/js/jquery-ui-1.8.2.custom.min.js"></script>
+<script type="text/javascript" src="/js/json2.min.js"></script>
+
+<link rel="stylesheet" href="/css/ladder.css" type="text/css" media="screen, projection">
+</head>
+<body>
+
+<div class="container">
+    <!-- Banner -->
+    <div class="span-24 last">
+    <div class="prepend-22 span-2 last"><a href='login.php?logout=1'>Logout</a></div>
+    <div class="prepend-1 span-10 append-13 last" >
+        <span id="header_title" style="font-size: 250%; vertical-align:bottom">Salem Table Tennis Ladder</span><br>
+    </div>
+    <div class="span-24 last">&nbsp;</div>
+
+    <!-- Toolbar -->
+    <div class="span-24 toolbar append-bottom last">
+    	<div class="prepend-2 span-7">
+    		<a href="ladder.php">Ladder</a>
+    	</div>
+    	<div class="prepend-1 span-7">
+    		<a href="settings.php">User Settings</a>
+    	</div>
+    	<div class="prepend-1 span-6 last">
+    		<a href="instructions.php">Instructions</a>
+    	</div>
+    </div>
+
+    <!-- Page Content -->
+    <?php $this->load->view($content_view); ?>
+
+</div>
+
+<?php
+
+// Footer
