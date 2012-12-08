2c505aa4b238fee6de72d64bcdc0de1003dfc0f8
Jim Kalafut <jim@kalafut.net>
2012-12-07 21:55:28 -0800

    Remove FirePHP

--- system/application/controllers/dashboard.php
+++ system/application/controllers/dashboard.php
@@ -17,8 +17,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once('FirePHPCore/FirePHP.class.php');
-
 class Dashboard extends Controller
 {
     private static $user;
